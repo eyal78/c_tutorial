2a77d643a0f3bcff8737628954fe22f559d51e40
Eyal <87993877+eyal78@users.noreply.github.com>
2022-05-27 18:46:01 +0300

    Create if_else.c

---
+++ if_else.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+int main()
+{
+   int x=5;
+   int y=6;
+   if(x>y)
+   {
+       printf("x>y");
+   } 
+   else 
+    if(x==y)
+        printf("y==x");
+    else
+        printf("y>x");
+}
