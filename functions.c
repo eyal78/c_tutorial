c23adc9d4ebc43c21bfe47411f5a267356498134
Eyal <87993877+eyal78@users.noreply.github.com>
2022-05-29 18:31:34 +0300

    Update functions.c

--- functions.c
+++ functions.c
@@ -1,1 +1,24 @@
 
+#include <stdio.h>
+
+//Function without parameters that does not return a value.
+void f1()
+{
+    printf("Good morning!\n");
+}
+//fromunction without parameters that returns a value
+int f2()
+{
+    int x=3;
+    int y=4;
+    return x+y;
+}
+//
+int main()
+{
+    int sum;
+    f1();
+    sum=f2();
+    printf("%d",sum);
+}
+
