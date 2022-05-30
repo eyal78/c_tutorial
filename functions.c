5d71cb84add8f05a89b131bdbf1893d58e18c671
Eyal <87993877+eyal78@users.noreply.github.com>
2022-05-30 11:00:13 +0300

    Update functions.c

--- functions.c
+++ functions.c
@@ -6,19 +6,26 @@ void f1()
 {
     printf("Good morning!\n");
 }
-//fromunction without parameters that returns a value
+//function without parameters that returns a value
 int f2()
 {
     int x=3;
     int y=4;
     return x+y;
 }
-//
+// function that accepts parameters and returns a value.
+int f3(char names[],int age)
+{
+    printf("hello %s, you are %d years old.\n",names,age);
+    return age/2;
+}
 int main()
 {
     int sum;
+    int half_age;
     f1();
     sum=f2();
-    printf("%d",sum);
+    printf("%d\n",sum);
+    half_age=f3("eyal",44);
+    
 }
-
